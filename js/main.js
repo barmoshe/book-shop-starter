0f9ea593cc63306d8d1b371b1428e45ce56d13ad
barmoshe <1barmoshe1@gmail.com>
2023-12-18 17:40:55 +0200

    before bonuses

--- js/main.js
+++ js/main.js
@@ -1,88 +1,194 @@
-'use strict'
+'use strict';
 
-let gBooks = _createBooks()
+let gBooks = _createBooks();
 
 function onInit() {
-  renderBooks()
+    gBooks = loadBooksFromStorage() || gBooks;
+    renderBooks();
 }
 
-async function renderBooks() {
-  console.log('books', gBooks)
-  const strHTMLs = gBooks.map((book) => {
-    return `<li class="book-preview">
-    <h4>${book.id}</h4>
-    <h4>${book.name}</h4>
-    <h4>${book.price}$</h4>
-    <div><img class="book-img" src="${book.imgUrl}"></div>
-    <div>
-      <button onclick="">Read</button>
-      <button onclick="">Update</button>
-      <button onclick="onRemoveBook('${book.id}')">Remove</button>
-    </div>
-  </li>`
-  })
-
-  const elBookList = document.querySelector('.book-list')
-  elBookList.innerHTML = strHTMLs.join('')
+function loadBooksFromStorage() {
+    const booksJSON = localStorage.getItem('books');
+    return JSON.parse(booksJSON);
 }
+
+function renderBooks(filteredBooks = gBooks) {
+    //if there are no books to display, add <p> tag with "No books to display" message
+    if (!filteredBooks.length) {
+        const elBookList = document.querySelector('.book-list');
+        elBookList.innerHTML = '<p>No books to display</p>';
+        return;
+    }
+    const strHTMLs = filteredBooks.map((book) => {
+        return `<li class="book-preview" data-id="${book.id}" >
+        <h4>${book.id}</h4>
+        <h4>${book.name}</h4>
+        <h4>${book.price}$</h4>
+        <div><img class="book-img" src="${book.imgUrl}"></div>
+        <div>
+            <button onclick="onReadBook('${book.id}')" class="btn-read">Read</button>
+            <button onclick="onUpdateBook('${book.id}')" class="btn-update">Update</button>
+            <button onclick="onRemoveBook('${book.id}')" class="btn-remove">Remove</button>
+        </div>
+    </li>`;
+    });
+
+    const elBookList = document.querySelector('.book-list');
+    elBookList.innerHTML = strHTMLs.join('');
+    saveBooksToStorage();
+}
+
+function saveBooksToStorage() {
+    const booksJSON = JSON.stringify(gBooks);
+    localStorage.setItem('books', booksJSON);
+}
+
 
 function onRemoveBook(bookId) {
-  console.log('bookId', bookId)
-  // TODO: remove the book by the bookId. re-render the books list after removing it from the array
+    const bookIndex = gBooks.findIndex(book => book.id === bookId);
+    //prompt user to confirm "are you sure you want to delete this bookName ?"
+    const isSure = confirm(`Are you sure you want to delete ${gBooks[bookIndex].name}?`);
+    if (!isSure) return;
+    gBooks.splice(bookIndex, 1);
+    renderBooks();
 }
 
-// TODO: onAddBook() -> Get data (prompt/inputs) and add the book to the array. re-render the books list.
-// TODO: onUpdateBook() -> Get data (prompt/inputs) and update the book in the books array. re-render the books list.
-// TODO: Show a modal, and show the specific book details inside it.
-// TODO: Hide the modal
+function onAddBook() {
+    const bookName = prompt('Enter the book name:');
+    if (!bookName) return;
+    const bookPrice = prompt('Enter the book price:')
+    if (!bookPrice) return;
+    gBooks.push(_createBook(bookName, bookPrice));
+    renderBooks();
+}
 
-// Now go for the bonuses :)
+function onUpdateBook(bookId) {
+    const bookName = prompt('Enter the new book name:');
+    if (!bookName) return;
+    const bookPrice = prompt('Enter the new book price:');
+    if (!bookPrice) return;
+
+    const bookIndex = gBooks.findIndex(book => book.id === bookId);
+    gBooks[bookIndex].name = bookName;
+    gBooks[bookIndex].price = bookPrice;
+
+    renderBooks();
+}
+
+function onReadBook(bookId) {
+    const selectedBook = gBooks.find(book => book.id === bookId);
+    if (selectedBook) {
+        displayBookDetails(selectedBook);
+    }
+}
+
+function onFilterBooks() {
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredBooks = gBooks.filter(book => book.name.toLowerCase().includes(searchTerm));
+    //log the filtered books
+    console.log(filteredBooks);
+    renderBooks(filteredBooks);
+}
+
+
+function displayBookDetails(book) {
+    const modal = document.getElementById("bookDetailsModal");
+    const modalContent = modal.querySelector(".modal-content");
+
+    const modalHTML = `
+      <span class="hide-btn" onclick="closeModal()">&times;</span>
+      <h2>${book.name}</h2>
+      <img src="${book.imgUrl}" alt="${book.name}" />
+      <p>${book.desc}</p>
+      <p>Price: ${book.price}$</p>
+      <p>Rating: <span id="rating">${book.rate}</span></p>
+      <button onclick="changeRating('${book.id}', 'increment')">+</button>
+      <button onclick="changeRating('${book.id}', 'decrement')">-</button>
+  `;
+
+    modalContent.innerHTML = modalHTML;
+    modal.style.display = "block";
+}
+
+function changeRating(bookId, action) {
+    const book = gBooks.find(book => book.id === bookId);
+    if (book) {
+        if (action === 'increment' && book.rate < 10) {
+            book.rate++;
+        } else if (action === 'decrement' && book.rate > 0) {
+            book.rate--;
+        }
+
+        document.getElementById("rating").textContent = book.rate;
+    }
+}
+
+function closeModal() {
+    const modal = document.getElementById("bookDetailsModal");
+    modal.style.display = "none";
+}
 
 function _createBooks() {
-  const books = [
-    _createBook('Harry Potter', 200, '../img/harry_potter.jpeg'),
-    _createBook('How To Sketch', 120, '../img/how_to_sketch.jpg'),
-    _createBook('Animals 101', 120, '../img/101_animals.jpg'),
-    _createBook('JS secrets', 50, '../img/js.jpg'),
-    _createBook('Lion King', 90, '../img/lion-king.jpg'),
-    _createBook('Mastering CSS', 135, '../img/css.jpg'),
-  ]
-  return books
+    const books = [
+        _createBook('Harry Potter', 200, '../img/harry_potter.jpeg'),
+        _createBook('How To Sketch', 120, '../img/how_to_sketch.jpg'),
+        _createBook('Animals 101', 120, '../img/101_animals.jpg'),
+        _createBook('JS secrets', 50, '../img/js.jpg'),
+        _createBook('Lion King', 90, '../img/lion-king.jpg'),
+        _createBook('Mastering CSS', 135, '../img/css.jpg'),
+    ]
+    return books
 }
 
 function _createBook(name, price, imgUrl = '../img/default_book.jpg') {
-  return {
-    id: _makeId(),
-    name,
-    price,
-    imgUrl,
-    rate: 0,
-    desc: _makeLorem()
-  }
+    return {
+        id: _makeId(),
+        name,
+        price,
+        imgUrl,
+        rate: 0,
+        desc: _makeLorem()
+    }
 }
 
 function _makeId(length = 5) {
-  var txt = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (var i = 0; i < length; i++) {
-    txt += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return txt;
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
 }
 
 function _makeLorem(wordCount = 100) {
-  const words = [
-    "lorem", "ipsum", "dolor", "sit", "amet", "consectetur",
-    "adipiscing", "elit", "sed", "do", "eiusmod", "tempor",
-    "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua"
-  ];
+    const words = [
+        "lorem", "ipsum", "dolor", "sit", "amet", "consectetur",
+        "adipiscing", "elit", "sed", "do", "eiusmod", "tempor",
+        "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua"
+    ];
 
-  let loremText = "";
+    let loremText = "";
 
-  for (let i = 0; i < wordCount; i++) {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    loremText += words[randomIndex] + " ";
-  }
+    for (let i = 0; i < wordCount; i++) {
+        const randomIndex = Math.floor(Math.random() * words.length);
+        loremText += words[randomIndex] + " ";
+    }
 
-  return loremText.trim();
+    return loremText.trim();
 }
+
+function toggleHoverClass(bookId) {
+    const bookPreview = document.querySelector(`.book-preview[data-id="${bookId}"]`);
+    bookPreview.classList.toggle('hovered');
+}
+sr = ScrollReveal({
+    origin: 'top',
+    distance: '35px',
+    duration: 1000,
+    reset: false,
+});
+
+sr.reveal('.book-preview', {
+    interval: 200,
+});
